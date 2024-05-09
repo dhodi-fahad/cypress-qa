fe78f4a3a6f98aa82a7d39c65ad6d5baafce26f6
dhodi-fahad <dhodifahad@gmail.com>
2024-05-09 23:50:16 +0300

    add: report  and retry configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,19 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter:"cypress-mochawesome-reporter",
+    reporterOptions: {
+      overwrite: false,
+      html: false,
+      json: true,
+      reportDir: "cypress/reports",
+    },
+    video: true,
+    retries: {
+      runMode: 1,
+      openMode: 1
+    }
   },
 });
