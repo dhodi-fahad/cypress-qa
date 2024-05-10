ef1f33bb54dbaadbfbd120ae078c350b25e31a82
dhodi-fahad <dhodifahad@gmail.com>
2024-05-10 13:31:24 +0300

    add to cart tests

---
+++ cypress/e2e/inventory/add_to_cart.cy.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+// Test Case: Add to Cart Functionality
+// Steps:
+//  - Click on the "Add to Cart" button for a product.
+//  - Verify that the product is added to the shopping cart without any errors.
+//  - Ensure that the cart icon updates to reflect the added product and the total number of items in the cart
+
+describe('Add To Cart', ()=>{
+    before(()=>{
+        cy.visit(Cypress.env('baseURL'))
+        cy.login('standard_user', 'secret_sauce')
+    })
+
+    after(()=>{
+        cy.logout()
+    })
+
+    it('Verify that the product is added and removed from the shopping cart without any errors', ()=>{
+        cy.get('[data-test="inventory-item"]').eq(0).find('[class="pricebar"]')
+            .find('button').should('contain.text', 'Add to cart').click()
+            cy.get('[data-test="shopping-cart-badge"]').should('contain.text', '1')
+        cy.get('[data-test="inventory-item"]').eq(0).find('[class="pricebar"]')
+            .find('button').should('contain.text', 'Remove').click()
+        cy.get('[data-test="shopping-cart-link"]').should('not.contain.text', '1')
+    })
+})
